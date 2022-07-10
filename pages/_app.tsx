376bc305c0886e9ad3d2c2fca6c3bf18d2ba90c3
giofanny.mowoka@pashouses.id <giofanny.mowoka@pashouses.id>
2022-07-10 19:45:51 +0700

    create Error Boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import ErrorBoundary from "../core/utilities/ErrorBoundary";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
 
-export default MyApp
+export default MyApp;
